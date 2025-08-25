2aea3177c73ffbbaf7454e71921d5570713c9185
abdullahkhalid08 <hafizabdullahkhalid810@gmail.com>
2025-08-25 18:19:51 +0500

    Add files via upload

---
+++ 28.js
@@ -0,0 +1,76 @@
+//  // // Functions in JS
+
+
+// // Formal Parameters ==> Functions Banaty waqt diye jatay hain
+// function add_two_numbers(a, b){
+//     return a + b
+// }
+
+
+// // Aruguments ==> Functions call karty waqt diye jatay hain
+// console.log(add_two_numbers(7, 8))
+
+
+// function Greetings(){
+//     console.log("Salam!")
+// }
+
+// Greetings()
+
+
+
+// function abc(){
+//     return "Salam!"
+// }
+
+// // console.log(abc())
+// let x = abc()
+// console.log(x)
+
+
+// Functions
+
+
+// Without parameters
+function greet() {
+    return "Hellow!"
+}
+
+// With parameters
+function greet(name) {
+    return `Hellow ${name}`
+}
+
+
+// let x = 5
+// let y = 5
+// let z = x + y
+
+
+// function add_two(a, b){
+//     return a + b
+// }
+// console.log(add_two(50, 100))
+
+
+// function add_two(){
+//     return 10 + 5 // hard-coded
+// }
+
+// console.log(add_two())
+
+
+
+// function my_name(name){
+//     return `My name is ${name}`
+// }
+// my_name("Shaheer")
+
+
+// function sum(a, b){
+//     return a + b
+// }
+// let z = sum(4, 5)
+// console.log(z)
+
+// console.log(sum(4, 8))
