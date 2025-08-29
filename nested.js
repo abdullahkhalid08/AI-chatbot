b415a0dd9e6c897342dd4408b8ff78f7bd6fe3c0
abdullahkhalid08 <hafizabdullahkhalid810@gmail.com>
2025-08-29 18:44:12 +0500

    Add files via upload

---
+++ nested.js
@@ -0,0 +1,67 @@
+// let age = 25;
+// let hasBirthCertificate = true;
+
+// if (age >= 50) {
+//     // First condition passed
+//     if (hasBirthCertificate) {
+//         console.log("Admission mil gaya!");
+//     } else {
+//         console.log("Birth certificate laao, phir admission milega.");
+//     }
+// } else {
+//     console.log("Abhi aap bary ho, koi or si universety dekho.");
+// }
+
+
+
+// let nested_array = {
+//     country: "America",
+//     presedint: "Trump",
+//     states: 50
+// }
+// console.log (nested_array);
+
+
+
+
+// let  nested_array = [
+//      ["Shaeer", "Pakistan", "Karachi",],
+//      ["Computer", "Maths", "urdu",],
+      
+
+// {
+   
+//    country: "America",
+//    presedint: "Trump",
+//    states: 50
+
+// }
+// ]
+// console.log(nested_array[2])
+
+// console.log(nested_array[2][2])
+
+// nested_array[2].national_language = "English"
+// nested_array[3]
+
+
+
+
+// // let nested_array = [
+// //     ["Shaeer", "Pakistan", "Karachi"],
+// //     ["Computer", "Maths", "Urdu"],
+// //     {
+// //         country: "America",
+// //         president: "Trump",
+// //         states: 50
+// //     }
+// // ];
+
+// // // Accessing the object
+// // console.log("Object at index 2:", nested_array[2]);
+
+// // // Accessing a value inside the object (e.g., states)
+// // console.log("Number of states:", nested_array[2].states);
+
+// // // Accessing an element from inner array
+// // console.log("Second subject:", nested_array[1][1]);  // "Maths"
