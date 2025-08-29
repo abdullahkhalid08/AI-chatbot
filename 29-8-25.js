5b89658657dee70210cca471017d58c781477a70
abdullahkhalid08 <hafizabdullahkhalid810@gmail.com>
2025-08-29 17:39:07 +0500

    Add files via upload

---
+++ 29-8-25.js
@@ -0,0 +1,15 @@
+// java scripit
+
+// let my_bike= {
+//     model: 55,
+//     variant: "cd 70",
+//    color:"blue"
+// }
+
+// console.log(my_bike)
+
+// my_bike.varient = "cd 125"
+// console.log(my_bike)
+
+// my_bike.genuine = True
+// console.log(my_bike)
